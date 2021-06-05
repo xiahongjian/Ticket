d76a3ef2bc0faf1292455fc5bb0a07e2e5ce0af7
xiahongjian <hongjian.xia@qq.com>
2021-06-05 14:37:27 +0800

    :tada: 初始化提交

---
+++ order/src/main/java/tech/hongjian/ticket/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package tech.hongjian.ticket.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+}
