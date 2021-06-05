aa2aba5f063383b195aa629ee606297b73746bfd
xiahongjian <hongjian.xia@qq.com>
2021-06-06 00:58:50 +0800

    依赖调整

--- member/src/main/java/tech/hongjian/ticket/member/feign/CouponFeignService.java
+++ member/src/main/java/tech/hongjian/ticket/member/feign/CouponFeignService.java
@@ -1,7 +1,6 @@
 package tech.hongjian.ticket.member.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import tech.hongjian.ticket.common.utils.R;
@@ -9,10 +8,9 @@ import tech.hongjian.ticket.common.utils.R;
 /**
  * Created by xiahongjian on 2021/6/5.
  */
-@Service
 @FeignClient("coupon")
 public interface CouponFeignService {
 
     @GetMapping("/coupon/coupon")
-    R listMemberCoupons(@RequestParam Integer memberId);
+    R listMemberCoupons(@RequestParam("memberId") Integer memberId);
 }
