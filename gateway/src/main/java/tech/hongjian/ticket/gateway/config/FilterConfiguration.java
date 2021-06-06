27699955d2353b3febe492ff1307ab410253e640
xiahongjian <hongjian.xia@qq.com>
2021-06-06 23:27:25 +0800

    网关路由配置

---
+++ gateway/src/main/java/tech/hongjian/ticket/gateway/config/FilterConfiguration.java
@@ -0,0 +1,30 @@
+package tech.hongjian.ticket.gateway.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+/**
+ * Created by xiahongjian on 2021/6/6.
+ */
+@Configuration
+public class FilterConfiguration {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.setAllowCredentials(true);
+
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return new CorsWebFilter(source);
+    }
+}
